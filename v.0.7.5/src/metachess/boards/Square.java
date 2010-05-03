f20c57d3dac038ca5e21cd37402b45f18b638f63
Jan Keromnes <janx@linux.com>
2010-05-03 11:06:27 +0000

    removed unused import
    git-svn-id: http://svn.code.sf.net/p/metachess/code@18 6a78e1aa-7590-4fe4-85e5-3479c06a842a

--- v.0.7.5/src/metachess/boards/Square.java
+++ v.0.7.5/src/metachess/boards/Square.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
-import metachess.game.Piece;
 import metachess.library.Colour;
 
 public class Square extends JButton {
