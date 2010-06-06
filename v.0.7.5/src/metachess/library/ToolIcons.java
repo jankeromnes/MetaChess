c69b9977f090cefe43803d54165fe8102d430741
jan333 <jan333@6a78e1aa-7590-4fe4-85e5-3479c06a842a>
2010-06-06 11:35:00 +0000

    added icons
    git-svn-id: https://svn.code.sf.net/p/metachess/code@38 6a78e1aa-7590-4fe4-85e5-3479c06a842a

--- v.0.7.5/src/metachess/library/ToolIcons.java
+++ v.0.7.5/src/metachess/library/ToolIcons.java
@@ -2,9 +2,14 @@ package metachess.library;
 
 public enum ToolIcons {
 
-    LEFT_ARROW("../resources/images/icons/left_arrow.gif"),
-	GOTO_ARROW("../resources/images/icons/goto_arrow.png"),
-	RIGHT_ARROW("../resources/images/icons/right_arrow.gif");
+    LEFT_ARROW("../resources/images/icons/back.png"),
+	RIGHT_ARROW("../resources/images/icons/next.png"),
+	GAME("../resources/images/icons/Wking.png"),
+	NEW("../resources/images/icons/add.png"),
+	SAVE("../resources/images/icons/accept.png"),
+	LOAD("../resources/images/icons/down.png"),
+	EXIT("../resources/images/icons/remove.png"),
+	BUILDER("../resources/images/icons/process.png");
 
    private String link;
 
