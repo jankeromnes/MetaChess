9562a28968703fbceca21787f6fff312611f16dc
Jan Keromnes <janx@linux.com>
2010-11-14 01:18:05 +0000

    Data Extractor update
    git-svn-id: http://svn.code.sf.net/p/metachess/code@67 6a78e1aa-7590-4fe4-85e5-3479c06a842a

---
+++ v.0.8.x/metachess/exceptions/ExtractException.java
@@ -0,0 +1,16 @@
+package metachess.exceptions;
+
+/** Exception that may occur during the data extraction process
+ * @author Jan (7DD)
+ * @version 0.8.5
+ */
+public abstract class ExtractException extends MetachessException {
+
+    /** Create an extract exception
+     * @param msg the explanation about the exception
+     */
+    public ExtractException(String msg) {
+	super("Extract Exception >> "+msg+" !");
+    }
+
+}
---
+++ v.0.8.x/metachess/exceptions/GlobalExtractException.java
@@ -0,0 +1,18 @@
+package metachess.exceptions;
+
+import metachess.library.Resource;
+
+/** Global exception in extraction process
+ * @author Jan (7DD)
+ * @version 0.8.4
+ */
+public class GlobalExtractException extends ExtractException {
+
+    /** Create a file load exception
+     * @param file the name of the file
+     */
+    public GlobalExtractException(String source, String destination) {
+	super("Could not extract from '"+source+"' to '"+destination+"'");
+    }
+
+}
