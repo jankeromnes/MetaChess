c56a474a7d339a44e3fd352b00935b2d51e92a28
agbeladem <agbeladem@6a78e1aa-7590-4fe4-85e5-3479c06a842a>
2010-11-20 21:56:00 +0000

    Saved games are now text files. Can load such files. (v.0.8.6 [1/4])
    git-svn-id: https://svn.code.sf.net/p/metachess/code@72 6a78e1aa-7590-4fe4-85e5-3479c06a842a

--- v.0.8.x/metachess/dialog/FileBox.java
+++ v.0.8.x/metachess/dialog/FileBox.java
@@ -7,22 +7,28 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 import metachess.game.Game;
 
+/** Class of the file load/save box
+ * @author Agbeladem (7DD)
+ * @version 0.8.6
+ */
 public class FileBox extends JFileChooser {
 
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	Game game;
-    boolean saving;
+    private static final long serialVersionUID = 1L;
+    private Game game;
+    private boolean saving;
 
+    /** Create a file box associated with a given game
+     * @param g the game
+     */
     public FileBox(Game g) {
 	game = g;
 	FileNameExtensionFilter f = new FileNameExtensionFilter("MetaChess Games (*.mcg)", "mcg");
 	setFileFilter(f);
     }
 
-
+    /** Launch this box
+     * @param s whether it was called to save (true) or to load (false)
+     */
     public void launch(boolean s) {
 	saving = s;
 	int ret;
@@ -38,8 +44,7 @@ public class FileBox extends JFileChooser {
 	    if(ret == JFileChooser.APPROVE_OPTION)
 		game.loadGame(getSelectedFile());
 	}
-
-
     }
+
 
 }
--- v.0.8.x/metachess/game/SavedGame.java
+++ v.0.8.x/metachess/game/SavedGame.java
@@ -3,27 +3,63 @@ package metachess.game;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-
+/** Class of a saved metachess game model
+ * @author Jan and Agbeladem (7DD)
+ * @version 0.8.6
+ */
 @SuppressWarnings("serial")
 public class SavedGame implements Serializable {
 
-	String setup;
-	boolean atomic;
-	int whiteAILevel;
-	int blackAILevel;
-	ArrayList<Move> moves;
-	
-	public SavedGame(String setup, boolean atomic, int whiteAILevel2, int blackAILevel2, ArrayList<Move> moves) {
-		this.setup = setup;
-		this.atomic = atomic;
-		this.whiteAILevel = whiteAILevel2;
-		this.blackAILevel = blackAILevel2;
-		this.moves = moves;
-	}
-	
-	public String getSetup() { return setup; }
-	public boolean isAtomic() { return atomic; }
-	public int getAILevel(boolean white) { return (white ? whiteAILevel : blackAILevel); }
-	public ArrayList<Move> getMoves() { return moves; }
+    private String setup;
+    private boolean atomic;
+    private int whiteAILevel;
+    private int blackAILevel;
+    private ArrayList<Move> moves;
+    
+    /** Create an empty saved game.
+     * Its properties will have to be defined by the set methods.
+     */
+    public SavedGame() {
+	moves = new ArrayList<Move>();
+    }
+    
+    /** Create a saved game
+     * @param setup the setup in which this game starts
+     * @param whiteAILevel2 the level of the white AI
+     * @param blackAILevel2 the level of the black AI
+     * @param moves the list of the moves played in this game, in the right order
+     */
+    public SavedGame(String setup, boolean atomic, int whiteAILevel2, int blackAILevel2, ArrayList<Move> moves) {
+	this.setup = setup;
+	this.atomic = atomic;
+	this.whiteAILevel = whiteAILevel2;
+	this.blackAILevel = blackAILevel2;
+	this.moves = moves;
+    }
+
+   
+    public void setSetup(String s) { setup = s; }
+    public void setAtomic(boolean a) { atomic = a; }
+    public void setBlackAILevel(int l) { blackAILevel = l; } 
+    public void setWhiteAILevel(int l) { whiteAILevel = l; } 
+
+    public boolean isAtomic() { return atomic; }    
+    public String getSetup() { return setup; }
+    public int getBlackAILevel() { return blackAILevel; }
+    public int getWhiteAILevel() { return whiteAILevel; }
+
+    /** Add a move to this saved game
+     * @param m the move to add
+     */
+    public void addMove(Move m) {
+	moves.add(m);
+    }
+
+    /** Get all the moves played in this saved game
+     * @return the list of the moves as an ArrayList
+     */ 
+    public ArrayList<Move> getMoves() {
+	return moves;
+    }
 
 }
