4a06c76570a170fc26dd8b6c1019fbf1152e6879
agbeladem <agbeladem@6a78e1aa-7590-4fe4-85e5-3479c06a842a>
2010-11-20 23:07:12 +0000

    42.
    git-svn-id: https://svn.code.sf.net/p/metachess/code@73 6a78e1aa-7590-4fe4-85e5-3479c06a842a

---
+++ v.0.8.x/metachess/loader/GameLoader.java
@@ -0,0 +1,86 @@
+package metachess.loader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StreamTokenizer;
+
+import metachess.exceptions.FileAccessException;
+import metachess.exceptions.FileContentException;
+import metachess.exceptions.LoadException;
+import metachess.game.SavedGame;
+
+/** Singleton of the Game Loader
+ * @author Agbeladem (7DD)
+ * @version 0.8.6
+ */
+public class GameLoader extends VariableLoader {
+
+    private static GameLoader instance = new GameLoader();
+
+    private SavedGame sg;
+
+    private boolean atomic;
+    private String setup;
+    private int whiteAILevel;
+    private int blackAILevel;
+
+    @Override
+    public void loadResource(String file) throws LoadException {
+	sg = new SavedGame();
+	try {
+	    this.file = file;
+	    BufferedReader br = new BufferedReader(new FileReader(file));
+
+	    String line = br.readLine();
+	    while(line.indexOf("{BEGIN}") == -1 && line != null) {
+		readVariable(line);
+		line = br.readLine();
+	    }
+
+	    StreamTokenizer st = new StreamTokenizer(br);
+	    st.eolIsSignificant(true);
+	    st.wordChars('0', '9');
+	    int next = st.nextToken();
+	    while(next != StreamTokenizer.TT_EOF) {
+		if(next == StreamTokenizer.TT_WORD) {
+		} else if(next != StreamTokenizer.TT_EOL)
+		    throw new FileContentException("Invalid token value : "+next, file);
+		next = st.nextToken();
+	    }
+	    br.close();
+	} catch(IOException e) {
+	    throw new FileAccessException(file);
+	}
+    }
+
+    protected void setVariable(String name, String value) throws FileContentException {
+
+	if(name.equals("atomic"))
+	    sg.setAtomic(value.equals("TRUE"));
+	else if(name.equals("setup"))
+	    sg.setSetup(value.toLowerCase());
+	else if(name.equals("whitelevel"))
+	    sg.setWhiteAILevel(Integer.parseInt(value));
+	else if(name.equals("blacklevel"))
+	    sg.setBlackAILevel(Integer.parseInt(value));
+ 	else throw new FileContentException("Unknown variable \""+name+'"', file);
+	
+    }
+
+    /** Get the saved game model that was loaded by this game loader
+     * @return the game model
+     */
+    public static SavedGame getSavedGame() {
+	return instance.sg;
+    }
+
+    /** Load a saved game
+     * @param file the file in which the game is saved
+     */
+    public static void load(File f) throws LoadException {
+	instance.loadResource(f.getPath());
+    }
+
+}
---
+++ v.0.8.x/metachess/loader/VariableLoader.java
@@ -0,0 +1,31 @@
+package metachess.loader;
+
+import metachess.exceptions.FileContentException;
+import metachess.exceptions.LoadException;
+
+/** Class of a loader that, among other things, load variables
+ * @author Agbeladem (7DD)
+ * @version 0.8.6
+ */
+public abstract class VariableLoader implements Loader {
+
+    protected String file;
+    protected abstract void setVariable(String name, String value) throws FileContentException;
+
+    protected void readVariable(String line) throws FileContentException {
+	int i = line.indexOf('#');
+	if(i != -1)
+	    line = line.substring(0,i);
+	line = line.replaceAll("\\s","");
+	i = line.indexOf('=');
+	if(i != -1) {
+	    String var = line.substring(0,i);
+	    String value = line.substring(i+1, line.length());
+	    setVariable(var.toLowerCase(), value.toUpperCase());
+	}
+    }
+
+    public abstract void loadResource(String s) throws LoadException;
+
+}
+
